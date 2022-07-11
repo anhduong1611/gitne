1fd919630cf343d9bbee910737d02ff2872595bc
Ho anh duong <hothianhduong16112002@hmail.com>
2022-07-11 15:20:43 +0700

    scane2

--- app/src/main/java/com/example/gitnenene/MainActivity.java
+++ app/src/main/java/com/example/gitnenene/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //koko
-        //koko
+        //koko1
         setContentView(R.layout.activity_main);
     }
 }
