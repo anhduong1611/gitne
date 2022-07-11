7742b33cd3a1c0ae8d34cf4d8275146fa5bc3db4
Ho anh duong <hothianhduong16112002@hmail.com>
2022-07-11 15:02:58 +0700

    hihi2

---
+++ app/src/main/java/com/example/gitnenene/Developne.java
@@ -0,0 +1,15 @@
+package com.example.gitnenene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Developne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developne);
+        //koko
+    }
+}
